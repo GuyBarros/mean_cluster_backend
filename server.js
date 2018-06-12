6cf7e8d483dd784da1192d4086222316b983764d
Guy Barros <guy@hashicorp.com>
2018-06-12 22:28:53 +0100

    initial backend commit

---
+++ server.js
@@ -0,0 +1,16 @@
+express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()
+)
+
+app.post('/api/message',function(req,res){
+    consol.log(req.body);
+    res.status(200);
+})
+
+var server = app.listen(5000,function(){
+    console.log('listening on port ', server.address().port)
+})
+
