6183eeda8372cc7e23ed206efaa8d3a9b81fa405
Guy Barros <guy@hashicorp.com>
2018-06-13 10:44:34 +0100

    code cleanup, added checkAuthenticated and cors services

--- server.js
+++ server.js
@@ -4,29 +4,28 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated');
+var cors = require('./services/cors');
 
+
+//MongoDb Connection
 mongoose.connect('mongodb://localhost:27017/test');
-
 var db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
   console.log("Connected successfully to server");
 });
 
+
+//Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-})
 
+//Requests
 app.get('/api/message',message.get)
-
-app.post('/api/message', message.post)
-
+app.post('/api/message',checkAuthenticated, message.post)
 app.post('/auth/register', auth.register)
 
 
